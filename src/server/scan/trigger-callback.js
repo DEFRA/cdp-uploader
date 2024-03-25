d76f119e678bc475fad5b1ba9a543678f975b435
Ivar Abrahamsen <ivar.abrahamsen@defra.gov.uk>
2024-03-25 14:59:07 +0000

    callback fetcher with errors

--- src/server/scan/trigger-callback.js
+++ src/server/scan/trigger-callback.js
@@ -1,11 +1,23 @@
 import { fetcher } from '~/src/server/common/helpers/fetcher'
+import { createLogger } from '~/src/server/common/helpers/logging/logger'
+
+const logger = createLogger()
 
 async function triggerCallback(url, payload) {
-  const callbackResponse = await fetcher(url, {
-    method: 'post',
-    body: JSON.stringify({ payload })
-  })
-  return callbackResponse
+  try {
+    const { json, response } = await fetcher(url, {
+      method: 'post',
+      body: JSON.stringify({ payload })
+    })
+    if (!response.ok) {
+      logger.error(new Error(`Failed to trigger callback ${url}, ${json}`))
+      return false
+    }
+    return true
+  } catch (error) {
+    logger.error(error)
+    return false
+  }
 }
 
 export { triggerCallback }
