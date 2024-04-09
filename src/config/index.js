0712f1ca3c8ce2f6f34571e871396dd6549682c8
chotai <chotai@users.noreply.github.com>
2024-04-09 17:50:26 +0100

    Replace s3 & sqs endpoint config with localstackEndpoint

--- src/config/index.js
+++ src/config/index.js
@@ -45,17 +45,10 @@ const config = convict({
     default: '/public',
     env: 'ASSET_PATH'
   },
-  sqsEndpoint: {
-    doc: 'AWS SQS endpoint',
+  localstackEndpoint: {
+    doc: 'Localstack endpoint',
     format: String,
-    default: 'http://127.0.0.1:4566',
-    env: 'SQS_ENDPOINT'
-  },
-  s3Endpoint: {
-    doc: 'AWS S3 endpoint',
-    format: String,
-    default: 'http://127.0.0.1:4566',
-    env: 'S3_ENDPOINT'
+    default: 'http://localhost:4566'
   },
   isProduction: {
     doc: 'If this application running in the production environment',
