4d4af23fd756bfba5a4a150e386af3b0019b7d91
chotai <chotai@users.noreply.github.com>
2024-04-10 13:55:18 +0100

    Update callback to not expect json response

--- src/server/callback/listener/handle-scan-results-callback.js
+++ src/server/callback/listener/handle-scan-results-callback.js
@@ -1,10 +1,10 @@
-import { fetcher } from '~/src/server/common/helpers/fetcher'
 import { toScanResultResponse } from '~/src/server/common/helpers/scan-result-response'
 import { deleteSqsMessage } from '~/src/server/common/helpers/sqs/delete-sqs-message'
 import {
   isAcknowledged,
   uploadStatus
 } from '~/src/server/common/helpers/upload-status'
+import fetch from 'node-fetch'
 
 async function handleScanResultsCallback(message, callbackQueueUrl, server) {
   try {
@@ -19,19 +19,23 @@ async function handleScanResultsCallback(message, callbackQueueUrl, server) {
     } else if (!isAcknowledged(uploadDetails.uploadStatus)) {
       const scanResult = toScanResultResponse(payload.uploadId, uploadDetails)
       const url = uploadDetails.scanResultCallbackUrl
-      const { json, response } = await fetcher(url, {
-        method: 'post',
-        body: JSON.stringify(scanResult)
-      })
+      server.logger.debug(payload, `Requesting callback to ${url}`)
+      const response = await fetchCallback(
+        url,
+        scanResult,
+        payload,
+        server.logger
+      )
 
-      if (response.ok) {
+      if (response && response.ok) {
         await deleteSqsMessage(server.sqs, callbackQueueUrl, receiptHandle)
         uploadDetails.uploadStatus = uploadStatus.acknowledged.description
-        await server.redis.storeUploadDetails(uploadDetails)
+        await server.redis.storeUploadDetails(payload.uploadId, uploadDetails)
+        server.logger.info(payload, `Callback to ${url} successful`)
       } else {
         server.logger.error(
           payload,
-          `Failed to trigger callback ${url}, ${json}`
+          `Failed to trigger callback ${url}, ${response.status}`
         )
       }
     } else if (isAcknowledged(uploadDetails.uploadStatus)) {
@@ -41,6 +45,20 @@ async function handleScanResultsCallback(message, callbackQueueUrl, server) {
   } catch (error) {
     server.logger.error(error)
   }
+}
+
+async function fetchCallback(url, scanResultResponse, sqsMessage, logger) {
+  let response
+  try {
+    response = await fetch(url, {
+      method: 'post',
+      body: JSON.stringify(scanResultResponse),
+      headers: { 'Content-Type': 'application/json' }
+    })
+  } catch (error) {
+    logger.error(sqsMessage, `Could not make callback. Error: ${error}`)
+  }
+  return response
 }
 
 export { handleScanResultsCallback }
