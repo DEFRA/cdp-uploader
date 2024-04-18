bf75b00ece00e2a43cd4a26a89fe88455e64e414
Ben Chidgey <ben@feedmypixel.com>
2024-04-18 17:21:16 +0100

    Redact 'uploadDetails' and 'uploadId' when not development
    - Redact 'req' when development
    - Remove hooks work

--- src/server/common/helpers/logging/logger-options.js
+++ src/server/common/helpers/logging/logger-options.js
@@ -2,33 +2,29 @@ import ecsFormat from '@elastic/ecs-pino-format'
 
 import { config } from '~/src/config'
 
-const hooks = {
-  logMethod(inputArgs, method, level) {
-    if (inputArgs.length === 2 && typeof inputArgs[0] === 'object') {
-      const arg1 = inputArgs.shift()
+const isDevelopment = config.get('isDevelopment')
+const redactionPaths = [
+  'req.headers.authorization',
+  'req.headers.cookie',
+  'res.headers'
+]
 
-      const loggingContext = {
-        uploadId: arg1.uploadId,
-        uploadStatus: arg1.uploadStatus,
-        fileIds: arg1.fileIds
-      }
-      return method.apply(this, [loggingContext, ...inputArgs])
-    } else {
-      return method.apply(this, inputArgs)
-    }
-  }
+if (!isDevelopment) {
+  redactionPaths.push('uploadDetails', 'uploadId')
 }
+
+if (isDevelopment) {
+  redactionPaths.push('req')
+}
+
 const loggerOptions = {
   enabled: !config.get('isTest'),
   redact: {
-    paths: ['req.headers.authorization', 'req.headers.cookie', 'res.headers'],
+    paths: redactionPaths,
     remove: true
   },
-  hooks,
   level: config.get('logLevel'),
-  ...(config.get('isDevelopment')
-    ? { transport: { target: 'pino-pretty' } }
-    : ecsFormat())
+  ...(isDevelopment ? { transport: { target: 'pino-pretty' } } : ecsFormat())
 }
 
 export { loggerOptions }
