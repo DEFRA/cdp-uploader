c2e4f6af0741aa6929768b6f8296fb71b331fe4b
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-05-15 14:36:21 +0100

    Update journey-tests.yml

--- compose/start-localstack.sh
+++ compose/start-localstack.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 export AWS_REGION=eu-west-2
+export AWS_DEFAULT_REGION=eu-west-2
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
 
-awslocal s3 mb s3://cdp-uploader-quarantine
-awslocal s3 mb s3://my-bucket
+aws --endpoint-url=https://localhost:4566 --no-verify-ssl s3 mb s3://cdp-uploader-quarantine
+aws --endpoint-url=https://localhost:4566 --no-verify-ssl s3 mb s3://my-bucket
 
 # queues
-awslocal sqs create-queue --queue-name cdp-clamav-results
-awslocal sqs create-queue --queue-name cdp-uploader-scan-results-callback.fifo --attributes "{\"FifoQueue\":\"true\",\"ContentBasedDeduplication\": \"true\"}"
+aws --endpoint-url=https://localhost:4566 --no-verify-ssl sqs create-queue --queue-name cdp-clamav-results
+aws --endpoint-url=https://localhost:4566 --no-verify-ssl sqs create-queue --queue-name cdp-uploader-scan-results-callback.fifo --attributes "{\"FifoQueue\":\"true\",\"ContentBasedDeduplication\": \"true\"}"
 
 # test harness
-awslocal sqs create-queue --queue-name mock-clamav
-awslocal s3api put-bucket-notification-configuration\
+aws --endpoint-url=https://localhost:4566 --no-verify-ssl sqs create-queue --queue-name mock-clamav
+aws --endpoint-url=https://localhost:4566 --no-verify-ssl s3api put-bucket-notification-configuration\
     --bucket cdp-uploader-quarantine \
     --notification-configuration '{
                                       "QueueConfigurations": [
