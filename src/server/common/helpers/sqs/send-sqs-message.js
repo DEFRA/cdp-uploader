559fd53455f9d18e8a3a814bf68eb7df2810a9f9
chotai <chotai@users.noreply.github.com>
2024-08-14 17:08:05 +0100

    Add MessageDeduplicationId to callback messages

--- src/server/common/helpers/sqs/send-sqs-message.js
+++ src/server/common/helpers/sqs/send-sqs-message.js
@@ -1,13 +1,28 @@
 import { SendMessageCommand } from '@aws-sdk/client-sqs'
 import crypto from 'node:crypto'
 
-async function sendSqsMessage(sqs, queue, messageBody, messageAttributes = {}) {
+/**
+ * @param sqs
+ * @param queue
+ * @param messageBody
+ * @param messageDeduplicationId
+ * @param messageAttributes
+ * @returns {Promise<*>}
+ */
+async function sendSqsMessage(
+  sqs,
+  queue,
+  messageBody,
+  messageDeduplicationId,
+  messageAttributes = {}
+) {
   return await sqs.send(
     new SendMessageCommand({
       QueueUrl: queue,
       MessageAttributes: messageAttributes,
       MessageBody: JSON.stringify(messageBody),
-      MessageGroupId: `cdp-uploader-${crypto.randomUUID()}`
+      MessageGroupId: `cdp-uploader-${crypto.randomUUID()}`,
+      MessageDeduplicationId: messageDeduplicationId
     })
   )
 }
