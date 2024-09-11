276f96e08ea97b15536b4379059ddd3b3254917c
Ben Chidgey <ben@feedmypixel.com>
2024-09-11 11:15:07 +0100

    Update health controller test

--- src/server/health/controller.test.js
+++ src/server/health/controller.test.js
@@ -1,17 +1,32 @@
-import { healthController } from '~/src/server/health/controller.js'
+import { createServer } from '~/src/server/index.js'
 
 describe('#healthController', () => {
-  const mockViewHandler = {
-    response: jest.fn().mockReturnThis(),
-    code: jest.fn().mockReturnThis()
-  }
+  /** @type {Server} */
+  let server
 
-  test('Should provide expected response', () => {
-    healthController.handler(null, mockViewHandler)
+  beforeAll(async () => {
+    server = await createServer()
+    await server.initialize()
+  })
 
-    expect(mockViewHandler.response).toHaveBeenCalledWith({
-      message: 'success'
+  afterAll(async () => {
+    server.events.emit('closing', { abort: true })
+    server.events.emit('stop')
+
+    await server.stop({ timeout: 0 })
+  })
+
+  test('Should provide expected response', async () => {
+    const { result, statusCode } = await server.inject({
+      method: 'GET',
+      url: '/health'
     })
-    expect(mockViewHandler.code).toHaveBeenCalledWith(200)
+
+    expect(result).toEqual({ message: 'success' })
+    expect(statusCode).toBe(200)
   })
 })
+
+/**
+ * @import { Server } from '@hapi/hapi'
+ */
