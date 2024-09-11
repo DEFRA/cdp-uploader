56a2685966d22fed7749615787517b23eba4523f
Ben Chidgey <ben@feedmypixel.com>
2024-09-11 11:12:42 +0100

    Remove messageAttributeNames All
    There are no custom message attrtibute names on this queue. All is also
    not a valid string

--- src/server/common/helpers/sqs/sqs-listener.js
+++ src/server/common/helpers/sqs/sqs-listener.js
@@ -24,7 +24,6 @@ const sqsListener = {
 
       const listener = Consumer.create({
         queueUrl,
-        messageAttributeNames: ['All'],
         waitTimeSeconds: options.config.waitTimeSeconds,
         pollingWaitTimeMs: options.config.pollingWaitTimeMs,
         shouldDeleteMessages: false,
