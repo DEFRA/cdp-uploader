5922c18d85231e1d2701151fda542c0ac7ce209a
Phil Segal <phil@phisec.co.uk>
2025-06-27 13:20:16 +0100

    CORE-919: Run E2E tests in github action

--- compose/01-start-localstack.sh
+++ compose/01-start-localstack.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+
+echo "[INIT SCRIPT] Starting LocalStack setup" >&2
+
 export AWS_REGION=eu-west-2
 export AWS_DEFAULT_REGION=eu-west-2
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
 
+echo "[INIT SCRIPT] Creating buckets" >&2
+
 aws --endpoint-url=http://localhost:4566 s3 mb s3://cdp-uploader-quarantine
 aws --endpoint-url=http://localhost:4566 s3 mb s3://my-bucket
+
+echo "[INIT SCRIPT] Creating queues" >&2
 
 # queues
 aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name cdp-clamav-results
---
+++ compose/99-ready.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo READY > /tmp/READY
